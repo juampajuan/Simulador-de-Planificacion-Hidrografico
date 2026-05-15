d28cafd8963b6a1e7ca7cc9c09de97db1f92e28d
Julen Gaumard <julengaumard@gmail.com>
2026-05-15 16:42:03 -0300

    Agrego modulo server

---
+++ server/src/requests/endpoints/mod.rs
@@ -0,0 +1,2 @@
+pub mod webpage;
+pub mod users;
---
+++ server/src/requests/handler.rs
@@ -0,0 +1,55 @@
+use tiny_http::{Server, Request, Method};
+use crate::structs::request::{RequestLog, HandlerResult};
+use crate::requests::endpoints::{webpage, users};
+
+const API_V1: &str = "/api/v1";
+
+/// Recibe todas las requests y llama al metodo que corresponde
+// Cada metodo, devuelve una response
+// Que luego es enviada por el sender y logueada
+pub fn handle_request(request: Request) -> RequestLog {
+ 
+    // Si se agrega otro versionado de apis, es tan facil, como agregar el `elseif` correspondiente.
+    let result = if let Some(api_path) = request.url().strip_prefix(API_V1) {
+
+        // AYUDA: En este match se agrega cada endpoint nueveo.
+        // El de users, es un ejemplo a eliminar. 
+        match (request.method(), api_path) {
+            (Method::Get, "/users") => users::get_users(&request),
+            _ => webpage::not_found(),
+        }
+
+    } else {
+        webpage::get_page_file(&request)
+    };
+
+    // ---- http//sdadad.com/ --> la pagina
+    // ---- http//sdadad.com/api/v1/users --> GET all users
+
+    response_sender(request, result)
+} 
+
+/// Envia la respuesta al cliente y la loguea por consola.
+// Si falla, imprime 499 como status code.
+fn response_sender(request: Request, result: HandlerResult) -> RequestLog {
+
+    let method = request.method().to_string();
+    let path = request.url().to_string();
+
+    let (response, status) = result;
+    let status_code = match request.respond(response) {
+        Ok(_) => status,
+        Err(_) => 499,
+    };
+
+    RequestLog{
+        method,
+        path,
+        status_code,
+    }
+}
+
+pub fn create_server() -> Result<Server, Box<dyn std::error::Error + Send + Sync>> {
+    let server = Server::http("0.0.0.0:3000")?;
+    Ok(server)
+}
