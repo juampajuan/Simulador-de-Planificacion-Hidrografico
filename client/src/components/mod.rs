63e4fbfbeb6084cf95c8d7d806b36db4cc926082
Julen Gaumard <julengaumard@gmail.com>
2026-05-18 17:57:09 -0300

    Agregados componentes y establecidas firmas, para organizar

---
+++ client/src/components/mod.rs
@@ -0,0 +1,6 @@
+// Aca agregas cada componente GENERICO
+// Es decir que se usa en multiples rutas.
+pub mod title;
+
+    // Cada archivo podria tener varios.
+    // Ese podria llamarse Titles o Texts, y tener los relacionados a eso
---
+++ client/src/requests.rs
@@ -0,0 +1,5 @@
+pub static API_URL: &str = "http://localhost:8080/api/v1";
+
+// TODO: Hacer un metodo para las requests aca.
+// Asi esta separado del resto, solamente lo llamas.
+// pub fn handle_request(method: unEnum, url, parametros) -> Option<> 
