2cbda4ec207f7e2ce00c21f4204c1d3befeb057f
epileF <fgazcon@fi.uba.ar>
2026-05-26 12:12:52 -0300

    primer intento fix

--- simulations/src/main.rs
+++ simulations/src/main.rs
@@ -45,37 +45,37 @@ fn main() {
     // }
 
     // --- Puntos perpendiculares ---
-    // let mut puntos_perpendiculares = Vec::new();
-    // let mut previous_point = &(puntos_a_medir[0]);
-    // let mut current_point = &(puntos_a_medir[0]);
-    // for punto in &puntos_a_medir {
-    //     let group = get_points_perpendicular_to_this(Some(previous_point), current_point, Some(punto), 2.5);
-    //     puntos_perpendiculares.extend(group);
-    //     previous_point = current_point;
-    //     current_point = punto;
-    // }
-    // let last_group = get_points_perpendicular_to_this(Some(previous_point), current_point, None, 2.5);
-    // puntos_perpendiculares.extend(last_group);
+    let mut puntos_perpendiculares = Vec::new();
+    let mut previous_point = &(puntos_a_medir[0]);
+    let mut current_point = &(puntos_a_medir[0]);
+    for punto in &puntos_a_medir {
+        let group = get_points_perpendicular_to_this(Some(previous_point), current_point, Some(punto), 2.5);
+        puntos_perpendiculares.extend(group);
+        previous_point = current_point;
+        current_point = punto;
+    }
+    let last_group = get_points_perpendicular_to_this(Some(previous_point), current_point, None, 2.5);
+    puntos_perpendiculares.extend(last_group);
 
-    //let medidas = processing::measuring::get_measures(MeasureMode::Circular { radius: (5.0) }, &matrix, &puntos_a_medir);
+    let medidas = processing::measuring::get_measures(MeasureMode::Circular { angle: (10.0) }, &matrix, &puntos_a_medir);
 
-    //for i in 0..ancho{
+    // for i in 0..ancho{
     //     for j in 0..altura {
     //         println!("{}, {}. {}", i, j, medidas[i][j]);
     //     }
     // }
 
 
-    // // --- Puntos circulares ---
-    // let mut puntos_circulares = Vec::new();
-    // for punto in &puntos_a_medir {
-    //     let group = get_points_circular_to_this(punto, 2.5);
-    //     puntos_circulares.extend(group);
-    // }
+    // --- Puntos circulares ---
+    let mut puntos_circulares = Vec::new();
+    for punto in &puntos_a_medir {
+        let group = get_points_circular_to_this(punto, 20.0, &matrix);
+        puntos_circulares.extend(group);
+    }
  
     // // --- Medidas ---
     let full_matrix_perp   = get_measures(MeasureMode::Perpendicular { step_distance: 2.5 }, &matrix, &puntos_a_medir);
-    //let full_matrix_circle = get_measures(MeasureMode::Circular { radius: 10.0 },            &matrix, &puntos_a_medir);
+    let full_matrix_circle = get_measures(MeasureMode::Circular { angle: 20.0 }, &matrix, &puntos_a_medir);
 
     // // --- Escritura de archivos ---
 
@@ -94,39 +94,39 @@ fn main() {
         writeln!(points_file, "({}, {})", x, y).expect("No se pudo escribir en points.txt");
     }
  
-    // let mut perp_file = File::create("perp.txt").expect("No se pudo crear perp.txt");
-    // for (x, y) in puntos_perpendiculares {
-    //     writeln!(perp_file, "({}, {})", x, y).expect("No se pudo escribir en perp.txt");
-    // }
+    let mut perp_file = File::create("perp.txt").expect("No se pudo crear perp.txt");
+    for (x, y) in puntos_perpendiculares {
+        writeln!(perp_file, "({}, {})", x, y).expect("No se pudo escribir en perp.txt");
+    }
  
-    // let mut circ_file = File::create("circ.txt").expect("No se pudo crear circ.txt");
-    // for (x, y) in puntos_circulares {
-    //     writeln!(circ_file, "({}, {})", x, y).expect("No se pudo escribir en circ.txt");
-    // }
+    let mut circ_file = File::create("circ.txt").expect("No se pudo crear circ.txt");
+    for (x, y) in puntos_circulares {
+        writeln!(circ_file, "({}, {})", x, y).expect("No se pudo escribir en circ.txt");
+    }
 
 
     
-    // // Solo escribe puntos con medida distinta de 0
-    // let mut measures_perp_file = File::create("measures_perp.txt").expect("No se pudo crear measures_perp.txt");
-    // for row in 0..full_matrix_perp.len() {
-    //     for col in 0..full_matrix_perp[row].len() {
-    //         let val = full_matrix_perp[row][col];
-    //         if val != 0.0 {
-    //             writeln!(measures_perp_file, "{},{},{:.4}", col, row, val); // col=x, row=y
-    //         }
-    //     }
-    // }
+    // Solo escribe puntos con medida distinta de 0
+    let mut measures_perp_file = File::create("measures_perp.txt").expect("No se pudo crear measures_perp.txt");
+    for row in 0..full_matrix_perp.len() {
+        for col in 0..full_matrix_perp[row].len() {
+            let val = full_matrix_perp[row][col];
+            if val != 0.0 {
+                writeln!(measures_perp_file, "{},{},{:.4}", col, row, val); // col=x, row=y
+            }
+        }
+    }
  
-    // let mut measures_circle_file = File::create("measures_circle.txt").expect("No se pudo crear.");
-    // for row in 0..full_matrix_circle.len() {
-    //     for col in 0..full_matrix_circle[row].len() {
-    //         let val = full_matrix_circle[row][col];
-    //         if val != 0.0 {
-    //             // Escribimos primero columna (X) y luego fila (Y) para que Python lo lea bien
-    //             writeln!(measures_circle_file, "{},{},{:.4}", col, row, val)
-    //                 .expect("No se pudo escribir en measures_circle.txt");
-    //         }
-    //     }
-    // }
+    let mut measures_circle_file = File::create("measures_circle.txt").expect("No se pudo crear.");
+    for row in 0..full_matrix_circle.len() {
+        for col in 0..full_matrix_circle[row].len() {
+            let val = full_matrix_circle[row][col];
+            if val != 0.0 {
+                // Escribimos primero columna (X) y luego fila (Y) para que Python lo lea bien
+                writeln!(measures_circle_file, "{},{},{:.4}", col, row, val)
+                    .expect("No se pudo escribir en measures_circle.txt");
+            }
+        }
+    }
 
 }
