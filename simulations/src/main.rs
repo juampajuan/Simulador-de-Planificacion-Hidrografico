cba9b4801fe6f7d88c15d4fdc3cf53301df9ca6d
epileF <fgazcon@fi.uba.ar>
2026-05-26 12:24:31 -0300

    cambio en monohaz: ahora recibe el angulo y el radio cubierto se calcula en base a la prof y el angulo

--- simulations/src/main.rs
+++ simulations/src/main.rs
@@ -19,8 +19,8 @@ fn main() {
         }
     };
     
-    let altura: usize = matrix.height;
-    let ancho: usize = matrix.width;
+    // let altura: usize = matrix.height;
+    // let ancho: usize = matrix.width;
 
 
     let recorrido = processing::routing::generate_route(
@@ -37,7 +37,7 @@ fn main() {
     //x,y
     //2045, 665
 
-    let puntos_a_medir = processing::measuring::find_measuring_points(&recorrido, 50.0);
+    let puntos_a_medir = processing::measuring::find_measuring_points(&recorrido, 100.0);
 
     
     // for point in puntos_a_medir{
@@ -45,19 +45,19 @@ fn main() {
     // }
 
     // --- Puntos perpendiculares ---
-    let mut puntos_perpendiculares = Vec::new();
-    let mut previous_point = &(puntos_a_medir[0]);
-    let mut current_point = &(puntos_a_medir[0]);
-    for punto in &puntos_a_medir {
-        let group = get_points_perpendicular_to_this(Some(previous_point), current_point, Some(punto), 2.5);
-        puntos_perpendiculares.extend(group);
-        previous_point = current_point;
-        current_point = punto;
-    }
-    let last_group = get_points_perpendicular_to_this(Some(previous_point), current_point, None, 2.5);
-    puntos_perpendiculares.extend(last_group);
+    // let mut puntos_perpendiculares = Vec::new();
+    // let mut previous_point = &(puntos_a_medir[0]);
+    // let mut current_point = &(puntos_a_medir[0]);
+    // for punto in &puntos_a_medir {
+    //     let group = get_points_perpendicular_to_this(Some(previous_point), current_point, Some(punto), 2.5);
+    //     puntos_perpendiculares.extend(group);
+    //     previous_point = current_point;
+    //     current_point = punto;
+    // }
+    // let last_group = get_points_perpendicular_to_this(Some(previous_point), current_point, None, 2.5);
+    // puntos_perpendiculares.extend(last_group);
 
-    let medidas = processing::measuring::get_measures(MeasureMode::Circular { angle: (10.0) }, &matrix, &puntos_a_medir);
+    // let medidas = processing::measuring::get_measures(MeasureMode::Circular { angle: (10.0) }, &matrix, &puntos_a_medir);
 
     // for i in 0..ancho{
     //     for j in 0..altura {
@@ -74,30 +74,29 @@ fn main() {
     }
  
     // // --- Medidas ---
-    let full_matrix_perp   = get_measures(MeasureMode::Perpendicular { step_distance: 2.5 }, &matrix, &puntos_a_medir);
+    // let full_matrix_perp   = get_measures(MeasureMode::Perpendicular { step_distance: 2.5 }, &matrix, &puntos_a_medir);
     let full_matrix_circle = get_measures(MeasureMode::Circular { angle: 20.0 }, &matrix, &puntos_a_medir);
 
     // // --- Escritura de archivos ---
 
-    let interpolacion = interpolate(InterpolationMethod::IDW,&puntos_a_medir, &full_matrix_perp, &matrix);
+    let interpolacion = interpolate(InterpolationMethod::IDW,&puntos_a_medir, &full_matrix_circle, &matrix);
 
     makePng_with_matrix_and_interpolation(&interpolacion, &matrix);
-    //process_depth(&matrix.data);
     
     let mut res_file = File::create("res.txt").expect("No se pudo crear res.txt");
     for (x, y) in recorrido {
         writeln!(res_file, "({}, {})", x, y).expect("No se pudo escribir en res.txt");
     }
  
-    let mut points_file = File::create("points.txt").expect("No se pudo crear points.txt");
-    for (x, y) in &puntos_a_medir {
-        writeln!(points_file, "({}, {})", x, y).expect("No se pudo escribir en points.txt");
-    }
+    // let mut points_file = File::create("points.txt").expect("No se pudo crear points.txt");
+    // for (x, y) in &puntos_a_medir {
+    //     writeln!(points_file, "({}, {})", x, y).expect("No se pudo escribir en points.txt");
+    // }
  
-    let mut perp_file = File::create("perp.txt").expect("No se pudo crear perp.txt");
-    for (x, y) in puntos_perpendiculares {
-        writeln!(perp_file, "({}, {})", x, y).expect("No se pudo escribir en perp.txt");
-    }
+    // let mut perp_file = File::create("perp.txt").expect("No se pudo crear perp.txt");
+    // for (x, y) in puntos_perpendiculares {
+    //     writeln!(perp_file, "({}, {})", x, y).expect("No se pudo escribir en perp.txt");
+    // }
  
     let mut circ_file = File::create("circ.txt").expect("No se pudo crear circ.txt");
     for (x, y) in puntos_circulares {
@@ -106,27 +105,27 @@ fn main() {
 
 
     
-    // Solo escribe puntos con medida distinta de 0
-    let mut measures_perp_file = File::create("measures_perp.txt").expect("No se pudo crear measures_perp.txt");
-    for row in 0..full_matrix_perp.len() {
-        for col in 0..full_matrix_perp[row].len() {
-            let val = full_matrix_perp[row][col];
-            if val != 0.0 {
-                writeln!(measures_perp_file, "{},{},{:.4}", col, row, val); // col=x, row=y
-            }
-        }
-    }
+    // // Solo escribe puntos con medida distinta de 0
+    // let mut measures_perp_file = File::create("measures_perp.txt").expect("No se pudo crear measures_perp.txt");
+    // for row in 0..full_matrix_perp.len() {
+    //     for col in 0..full_matrix_perp[row].len() {
+    //         let val = full_matrix_perp[row][col];
+    //         if val != 0.0 {
+    //             writeln!(measures_perp_file, "{},{},{:.4}", col, row, val); // col=x, row=y
+    //         }
+    //     }
+    // }
  
-    let mut measures_circle_file = File::create("measures_circle.txt").expect("No se pudo crear.");
-    for row in 0..full_matrix_circle.len() {
-        for col in 0..full_matrix_circle[row].len() {
-            let val = full_matrix_circle[row][col];
-            if val != 0.0 {
-                // Escribimos primero columna (X) y luego fila (Y) para que Python lo lea bien
-                writeln!(measures_circle_file, "{},{},{:.4}", col, row, val)
-                    .expect("No se pudo escribir en measures_circle.txt");
-            }
-        }
-    }
+    // let mut measures_circle_file = File::create("measures_circle.txt").expect("No se pudo crear.");
+    // for row in 0..full_matrix_circle.len() {
+    //     for col in 0..full_matrix_circle[row].len() {
+    //         let val = full_matrix_circle[row][col];
+    //         if val != 0.0 {
+    //             // Escribimos primero columna (X) y luego fila (Y) para que Python lo lea bien
+    //             writeln!(measures_circle_file, "{},{},{:.4}", col, row, val)
+    //                 .expect("No se pudo escribir en measures_circle.txt");
+    //         }
+    //     }
+    // }
 
 }
