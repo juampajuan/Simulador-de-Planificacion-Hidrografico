bc0664c20f4fe25c468ef761c013ad986bb5edc1
Julen Gaumard <julengaumard@gmail.com>
2026-06-05 13:40:09 -0300

    Agregas definiciones de tablas (v0) y los metodos para manipulars mediante el struct de la DB

---
+++ server/src/db/queries/professor.rs
@@ -0,0 +1,73 @@
+use sqlite::State;
+use crate::db::engine::DBEngine;
+
+pub fn create_professor(
+    db: &DBEngine,
+    username: &str,
+    password_hash: &str,
+) -> Result<usize, sqlite::Error> {
+
+    let mut statement = db.run_query(
+        "
+        INSERT INTO professors(username, password_hash)
+        VALUES(?, ?)
+        RETURNING id
+        "
+    )?;
+
+    statement.bind((1, username))?;
+    statement.bind((2, password_hash))?;
+
+    if let sqlite::State::Row = statement.next()? {
+        let id = statement.read::<i64, _>("id")? as usize;
+        Ok(id)
+    } else {
+        unreachable!()
+    }
+}
+
+pub fn change_password(
+    db: &DBEngine,
+    professor_id: i64,
+    new_password_hash: &str,
+) -> Result<(), sqlite::Error> {
+
+    let mut statement = db.run_query(
+        "
+        UPDATE professors
+        SET password_hash = ?
+        WHERE id = ?
+        "
+    )?;
+
+    statement.bind((1, new_password_hash))?;
+    statement.bind((2, professor_id))?;
+    statement.next()?;
+
+    Ok(())
+}
+
+
+pub fn get_professor_id_by_id(
+    db: &DBEngine,
+    professor_id: i64,
+) -> Result<Option<usize>, sqlite::Error> {
+
+    let mut statement = db.run_query(
+        "
+        SELECT id
+        FROM professors
+        WHERE id = ?
+        "
+    )?;
+
+    statement.bind((1, professor_id))?;
+
+    if let sqlite::State::Row = statement.next()? {
+        Ok(Some(
+            statement.read::<i64, _>("id")? as usize
+        ))
+    } else {
+        Ok(None)
+    }
+}
