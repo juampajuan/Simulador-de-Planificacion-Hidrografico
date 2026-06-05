c72c9152ac5189826752e133bcce981f6886dd09
Julen Gaumard <julengaumard@gmail.com>
2026-06-05 13:41:41 -0300

    Agregado thread de CLI y joins en main

--- server/src/threads/creators.rs
+++ server/src/threads/creators.rs
@@ -1,12 +1,18 @@
-use std::thread;
+use std::thread::{self, JoinHandle};
 use tiny_http::Request;
 use std::sync::{Arc, Mutex};
 use crate::structs::filecache::{FileCache};
 use crate::requests::handler::handle_request;
 
-pub fn create_request_thread(request: Request, cache: Arc<Mutex<FileCache>>) {
+pub fn create_request_thread(request: Request, cache: Arc<Mutex<FileCache>>) -> JoinHandle<()> {
     thread::spawn(move || {
         let log = handle_request(request, cache);
         log.print();
-    });
+    })
+}
+
+pub fn create_cli_thread(port: i32) -> JoinHandle<()> {
+    thread::spawn(move || {
+        cli::cli_server_menu(port);
+    })
 }
