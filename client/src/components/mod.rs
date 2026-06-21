bfdec4749e58b0e72dd1836938970bc8be14f19c
Julen Gaumard <julengaumard@gmail.com>
2026-06-21 19:00:13 -0300

    Agrego notice de NO responsive

--- client/src/components/mod.rs
+++ client/src/components/mod.rs
@@ -7,6 +7,7 @@ pub mod darkmode_btn;
 pub mod form_inputs;
 pub mod confirm_modal;
 pub mod modal;
+pub mod no_responsive;
 
 // Cada archivo podria tener varios.
 // Ese podria llamarse Titles o Texts, y tener los relacionados a eso
--- client/src/pages/not_found.rs
+++ client/src/pages/not_found.rs
@@ -1,15 +1,17 @@
 use yew::prelude::*; 
+use lucide_yew::Compass;
  
 #[function_component(NotFound)]
 pub fn not_found() -> Html {
 
     html! {
-        <div class="h-screen bg-zinc-900 text-white flex flex-col items-center justify-center">
-
-            <h1 class="text-5xl font-bold mb-8">
-                { "TE PERDISTE" }
-            </h1>
-
+        <div class="bg-slate-900 h-screen w-screen p-3 dark">
+            <div class="h-full dot-grid text-white flex flex-col items-center justify-center">
+                <Compass size={64} stroke_width={1usize}/>
+                <h1 class="text-3xl font-semibold pt-5 mb-8">
+                    { "Creemos que te perdiste." }
+                </h1>
+            </div>
         </div>
     }
 }
